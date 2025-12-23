dbf92b975fbae409505f719843c142f5baa87e0d
Lahari <laharisabbathi@gmail.com>
2025-12-23 11:38:26 +0530

    Update Reverse.java

--- 23-12-2025/Reverse.java
+++ 23-12-2025/Reverse.java
@@ -16,3 +16,38 @@ class Reverse
 		System.out.println("Reverse_Integer :"+ " " +rev);
 	}
 }
+
+
+#Reversing numbers in an array
+
+	
+import java.util.Scanner;
+class ReverseNum
+{
+	public static void main(String args[])
+	{
+		System.out.println("Enter the size of an array:");
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int arr[] = new int[n];
+		
+		System.out.println("Enter" +n + "numbers");
+		for(int i=0; i<n ; i++)
+		{
+			arr[i] = sc.nextInt();
+		}
+		
+		for(int i = 0; i< n ; i++)
+		{
+			int num = arr[i];
+			int rev = 0;
+			while(num>0)
+			{
+				int  d = num% 10;
+				rev = rev * 10 + d;
+				num = num / 10;
+			}
+			System.out.println("Reverse_Integer :"+ " " +rev);
+		}
+	}
+}
