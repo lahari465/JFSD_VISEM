942642bd475eb6154a0ab499ac5e2a5ef373ff9e
Lahari <laharisabbathi@gmail.com>
2025-12-23 11:13:25 +0530

    NumberSystem and Wrapper classes

---
+++ 23-12-2025/TestProg.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class TestProg
+{
+	public static void main(String args[])
+	{
+		String bin_s = "111110001";
+		int ans = Integer.parseInt(bin_s, 2);
+		System.out.println("bin_string :"  +bin_s + "\tDecimal : " +ans);
+
+		String oct_s = "12";
+		ans = Integer.parseInt(oct_s, 8);
+		System.out.println("oct_string :"  + oct_s + "\tDecimal : " +ans);
+
+		String hex_s = "12A";
+		ans = Integer.parseInt(hex_s, 16);
+		System.out.println("hex_string :" + hex_s + "\tDecimal : " +ans);
+
+		String dec_s = "12";
+		ans = Integer.parseInt(dec_s, 10);
+		System.out.println("dec_string :" + dec_s + "\tDecimal : " +ans);
+
+		
+		System.out.println("Enter a binary string:");
+		Scanner sc = new Scanner(System.in);
+		bin_s = sc.next();
+		int dec_ans = Integer.parseInt(bin_s,2);
+		System.out.println("bin_string: " +bin_s + "\tDecimal :" +dec_ans);	
+
+	}
+}
